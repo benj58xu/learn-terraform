e96c7339f84bac3a5e228cead98ccfceaefda7dd
Benjamin Xu <benj58xu@Benjamins-MacBook-Air.local>
2026-03-25 13:20:05 -0400

    Another try

--- terraform.tf
+++ terraform.tf
@@ -9,13 +9,19 @@ terraform {
   backend "s3" {
     bucket       = "benj58xu-terraform-backend"
     use_lockfile = true
-    region       = "us-east-2" /* always use this region for tfstate files */
+    region       = var.aws_region
     /* 1. Need to change this for the current stack */
     key = "will-be-overridden-by-command-line/terraform.tfstate"
   }
 
   required_version = ">= 1.2"
 }
+
+
+provider "aws" {
+  region = var.aws_region
+}
+
 
 /*
 You may need to run "terraform init -reconfigure" to forget .terraform/terraform.tfstate
