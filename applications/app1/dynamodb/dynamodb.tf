27d7cc196fda2b66acf1cf00824131e939e04b9e
Benjamin Xu <benj58xu@Benjamins-MacBook-Air.local>
2026-03-25 13:29:28 -0400

    Try again

--- applications/app1/dynamodb/dynamodb.tf
+++ applications/app1/dynamodb/dynamodb.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
 resource "aws_dynamodb_table" "example_table" {
   name         = "MusicCollection"
   billing_mode = "PAY_PER_REQUEST" # Use ON-DEMAND billing mode for simplicity and auto-scaling
