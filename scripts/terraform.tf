bf5e272e0f30797d48ab048cdd98a76b1691b1f6
Benjamin Xu <benj58xu@Benjamins-MacBook-Air.local>
2026-03-27 11:29:37 -0400

    Make terraform s3 bucket name dynamic

--- scripts/terraform.tf
+++ scripts/terraform.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "benj58xu-terraform-backend"
+    bucket       = var.bucket
     use_lockfile = true
     region       = var.aws_region     /* this is replaced by sed */
     key = "will-be-overridden-by-command-line/terraform.tfstate"
